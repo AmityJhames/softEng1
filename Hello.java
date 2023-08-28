b20f01811908aee39e493f2b95d1430bb775da6e
AmityJhames <143429028+AmityJhames@users.noreply.github.com>
2023-08-29 00:12:03 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String[] args){
+    system.out.println("Hello,Software Engineering 1!");
+    system.out.println(" Student Name: ABAJA,Amity Jhames");
+  }
+}

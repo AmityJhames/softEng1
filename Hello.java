b4cae8706dc48a2b7d4635e5cffbead20623a030
AmityJhames <amityjhames@gmail.com>
2023-09-04 20:01:05 +0800

    added a new file MyNewFile.txt.txt

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main(String[] args){
     system.out.println("Hello,Software Engineering 1!");
     system.out.println(" Student Name: ABAJA,Amity Jhames");
+    system.out.println("Complicated world of SoftDev that is why it pays much!");
   }
 }
